28236db83bf2fe378d6350922cf7b2f74d76ba4d
andres_betancur <65365066+18517701@users.noreply.github.com>
2023-05-19 22:31:47 -0500

    perfil profesor

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PerfilProfeComponent } from './pages/perfil-profe/perfil-profe.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home' , // if loading is add, change this "home" for loading path
+
     pathMatch: 'full'
   },
   {
     path: 'home',
     component: HomePageComponent,
+  },
+  {path:'perfil',
+  component: PerfilProfeComponent
   },
 ];
 
---
+++ frontend/src/app/pages/perfil-profe/perfil-profe.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { faAngleRight, faUser, faLocationDot,faStar } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-perfil-profe',
+  templateUrl: './perfil-profe.component.html',
+  styleUrls: ['./perfil-profe.component.css']
+})
+export class PerfilProfeComponent {
+faAngleRight= faAngleRight;
+faLocationDot = faLocationDot;
+faUser = faUser;
+faStar = faStar;
+
+}
