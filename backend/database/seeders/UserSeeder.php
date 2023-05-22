66f273533378cd19b734dd0bf7abfebd8205b4b9
M-Jairo <jairo03.hotmail@gmail.com>
2023-05-22 02:39:06 -0300

    cambios en seed userSeeder

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -24,10 +24,9 @@ class UserSeeder extends Seeder
                 'first_name' => fake()->firstName(),
                 'last_name' => fake()->lastName(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                'birthdate' => fake()->dateTimeBetween('-50 years', '-25 years')->format('Y-m-d'),
+                'birthday' => fake()->dateTimeBetween('-50 years', '-25 years')->format('Y-m-d'),
                 'identification' => fake('ar_SA')->idNumber,
                 'phone' => fake('en_US')->tollFreePhoneNumber(),
-                'photo' => null,
                 'city_id' => fake()->numberBetween(1,529),
                 'last_connection' => fake()->dateTimeBetween('-60 days', 'now')->format('Y-m-d H:i:s'),
                 // 'email_verified_at' => now(),
