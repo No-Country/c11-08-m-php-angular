b6b227aad46159d9c135b188ed559676e571b1d8
dianagv7 <diagvan7@gmail.com>
2023-05-29 13:13:49 -0500

    fake data reviews fixed

--- backend/database/seeders/RewiewSeeder.php
+++ backend/database/seeders/RewiewSeeder.php
@@ -17,17 +17,17 @@ class RewiewSeeder extends Seeder
      */
     public function run()
     {
-        $teachers = Teacher::all();
+        // $teachers = Teacher::all();
 
-        //por cada profesor crear n aleatorio de reviews
-        foreach($teachers as $teacher){
-            $nReview = random_int(1,20);
-            //para cada review tomar estudiante aleatorio existente pero que no se repita los estudiantes para un profesor
-            $students_id = fake()->randomElements(DB::table('students')->pluck('id')->toArray(), $nReview, false);
-            foreach($students_id as $student_id){
-                Review::factory()
-                ->create(['teacher_id' => $teacher->id, 'student_id' => $student_id]);
-            }
-        }
+        // //por cada profesor crear n aleatorio de reviews
+        // foreach($teachers as $teacher){
+        //     $nReview = random_int(1,20);
+        //     //para cada review tomar estudiante aleatorio existente pero que no se repita los estudiantes para un profesor
+        //     $students_id = fake()->randomElements(DB::table('students')->pluck('id')->toArray(), $nReview, false);
+        //     foreach($students_id as $student_id){
+        //         Review::factory()
+        //         ->create(['teacher_id' => $teacher->id, 'student_id' => $student_id]);
+        //     }
+        // }
     }
 }
