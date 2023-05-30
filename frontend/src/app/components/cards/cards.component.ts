e3d56d39f1dd2b88e872a921f1a137152542cc04
Juan Martin <jmartincubells@gmail.com>
2023-05-30 16:16:46 -0300

    recuperando suscription

--- frontend/src/app/components/cards/cards.component.ts
+++ frontend/src/app/components/cards/cards.component.ts
@@ -1,9 +1,7 @@
 import { Teacher } from './../../interfaces/teacher';
-import { TeacherService } from './../../services/teacher.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { faLocationDot, faStar } from '@fortawesome/free-solid-svg-icons';
-import { INewCard } from 'src/app/interfaces/card';
-import { CardService } from 'src/app/services/card.service';
+
 
 
 @Component({
