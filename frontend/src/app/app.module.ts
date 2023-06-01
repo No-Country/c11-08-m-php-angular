e2ff9a8d2025cdc90505b5d135d96e38bdad13b8
DanielMino19 <danielmino926@gmail.com>
2023-06-01 10:37:46 -0300

    testing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,9 @@
-
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxPaginationModule } from 'ngx-pagination';  //pagi
-
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardsComponent } from './components/cards/cards.component';
@@ -18,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
 import { PerfilProfeComponent } from './pages/perfil-profe/perfil-profe.component';
 
-
+import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { SubscriptionComponent } from './pages/subscription/subscription.component';
@@ -27,7 +25,7 @@ import { ConfirmDataComponent } from './pages/payment/confirm-data/confirm-data.
 import { FinishProfileComponent } from './pages/payment/finish-profile/finish-profile.component';
 import { PaymentComponent } from './pages/payment/payment/payment.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
@@ -42,8 +40,14 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     PerfilProfeComponent,
     LoginRegisterComponent,
     ScheduleComponent,
-    PerfilProfeComponent
-
+    PerfilProfeComponent,
+    RegisterComponent,
+    SubscriptionComponent,
+    CheckoutComponent,
+    ConfirmDataComponent,
+    FinishProfileComponent,
+    PaymentComponent
+    
   ],
   imports: [
     HttpClientModule,
