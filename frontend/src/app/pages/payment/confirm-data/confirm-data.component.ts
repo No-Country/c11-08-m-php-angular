8b837b799ee17e1a0eb99e07c0b193fe88dfcfd8
Your Name <jmartincubells@gmail.com>
2023-06-01 00:13:25 -0300

    flujo de registro del docente

--- frontend/src/app/pages/payment/confirm-data/confirm-data.component.ts
+++ frontend/src/app/pages/payment/confirm-data/confirm-data.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { ProvincesCity } from 'src/app/interfaces/provincesCity';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProvincesService } from 'src/app/services/provinces.service';
 
 @Component({
   selector: 'app-confirm-data',
@@ -7,5 +12,48 @@ import { Component } from '@angular/core';
 })
 export class ConfirmDataComponent {
   imgTeacher = '../../../../assets/images/subscription-page/nocountry (1).png';
+
+  
+  registerForm: FormGroup;
+  
+  
+  
+  
+  constructor(
+    private provincesService: ProvincesService,
+    private formBuilder: FormBuilder,
+    ) {
+      this.registerForm = this.formBuilder.group({
+        name: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      });
+    }
+    
+    listProvinces: ProvincesCity[] = [];
+  get name() {
+    return this.registerForm.get('name')
+  }
+
+  get lastName() {
+    return this.registerForm.get('lastName')
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  ngOnInit(): void {
+
+    this.getProvinces();
+  }
+  getProvinces(): void {
+    this.provincesService.getProvinces().subscribe(
+      res => {
+        this.listProvinces = res;
+      },
+      err => console.log(err)
+    );
+  }
 
 }
