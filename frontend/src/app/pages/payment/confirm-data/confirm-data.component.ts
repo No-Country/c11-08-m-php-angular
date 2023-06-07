1d021e7201d35abffc5c6050c8850c5d46b31710
Your Name <jmartincubells@gmail.com>
2023-06-07 11:25:57 -0300

    tt

--- frontend/src/app/pages/payment/confirm-data/confirm-data.component.ts
+++ frontend/src/app/pages/payment/confirm-data/confirm-data.component.ts
@@ -12,25 +12,27 @@ import { ProvincesService } from 'src/app/services/provinces.service';
 })
 export class ConfirmDataComponent implements OnInit {
   imgTeacher = '../../../../assets/images/subscription-page/nocountry (1).png';
-
-  
+  ruta: string = "/payment/finish-profile"
   confirmForm: FormGroup;
-  
-  
-  
-  
+
+
+
+
   constructor(
     private provincesService: ProvincesService,
     private formBuilder: FormBuilder,
-    ) {
-      this.confirmForm = this.formBuilder.group({
-        name: ['', Validators.required],
-        lastName: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-      });
-    }
-    
-    listProvinces: ProvincesCity[] = [];
+    private authService: AuthService,
+  ) {
+    this.confirmForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  UserData : any;
+
+  listProvinces: ProvincesCity[] = [];
   get name() {
     return this.confirmForm.get('name')
   }
@@ -44,9 +46,17 @@ export class ConfirmDataComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.authService.currentUser.subscribe(user => {
+      this.UserData = user;
+      this.userName = this.UserData.first_name;
+      this.userLastName = this.UserData.last_name;
+    })
     this.getProvinces();
   }
+
+  userName : string = '';
+  userLastName: string ='';
+
   getProvinces(): void {
     this.provincesService.getProvinces().subscribe(
       res => {
@@ -56,4 +66,28 @@ export class ConfirmDataComponent implements OnInit {
     );
   }
 
+  onEnviar(event: Event) {
+    //console.log(this.confirmForm);
+
+    /* event.preventDefault();
+    if (this.confirmForm.valid) {
+      console.log(JSON.stringify(this.confirmForm.value));
+      this.autenticarService.loginUser(this.confirmForm.value).subscribe(db => {
+        console.log("DATA: " + JSON.stringify(db.id));
+        if (db.id) {
+          alert("Puedes editar el portfolio");
+          this.ruta.navigate(['/dashboard']);
+          this.confirmForm.reset()
+        } else {
+          alert("Error al iniciar sesión, credenciales no válidas!!!");
+        }
+      }, err => {
+        alert("ERROR!!!");
+      })
+    } else {
+      sessionStorage.setItem('currentUser', "");
+      alert("Error! No tienes acceso");
+      this.ruta.navigate(['/']);
+    } */
+  } 
 }
