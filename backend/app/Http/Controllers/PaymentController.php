10936b8e1ea1cfc5f15b3c3abf9dd1776aa18967
JoaquinJS18 <jhinfire@gmail.com>
2023-06-08 21:51:42 -0300

    CRUD de pagos creado

--- backend/app/Http/Controllers/PaymentController.php
+++ backend/app/Http/Controllers/PaymentController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SubscriptionRequest;
 use App\Services\PaymentService;
 use Illuminate\Http\Request;
+use App\Http\Requests\PaymentRequest;
+use App\Http\Resources\PaymentResource;
+use App\Models\Payment;
 
 class PaymentController extends Controller
 {
@@ -18,27 +21,52 @@ class PaymentController extends Controller
 
     public function index()
     {
-        //
+        try {
+            $payments = $this->service->getPayments();
+            return PaymentResource::collection($payments);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'type' => 'error'],500);
+        }
     }
 
-    public function store(Request $request)
+    public function store(PaymentRequest $request)
     {
-        //
+        try {
+            $data = $this->service->createPayment($request->all());
+            return new PaymentResource($data);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'type' => 'error'],500);
+        }
     }
 
-    public function show($id)
+    public function show(Payment $payment)
     {
-        //
+        try {
+            $payment = $this->service->getPayment($payment);
+            return new PaymentResource($payment);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'type' => 'error'],500);
+        }
     }
 
-    public function update(Request $request, $id)
+    public function update(PaymentRequest $request, Payment $payment)
     {
-        //
+        try {
+            $this->service->updatePayment($request->all(), $payment);
+            return new PaymentResource($payment);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'type' => 'error'],500);
+        }
     }
 
-    public function destroy($id)
+    public function destroy(Payment $payment)
     {
-        //
+        try {
+            $this->service->deletePayment($payment);
+            return response()->json(['type' => 'success', 'message' => 'Eliminado exitosamente'],200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage(), 'type' => 'error'],500);
+        }
     }
 
     public function getSubscriptionLink(SubscriptionRequest $request)
